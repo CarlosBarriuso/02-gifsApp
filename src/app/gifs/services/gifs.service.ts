aac491861086ff95c1cb472a1d4ab0ab72031fe2
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-20 12:01:31 +0100

    Control de búsqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,6 +8,7 @@ export class GifsService {
   private _historial: string[] = []; //
 
   get historial(){
+    
     //Romper con la referencia original y tener un nuevo array, se usa el operador spread ...
     return [...this._historial];
   }
@@ -15,7 +16,18 @@ export class GifsService {
   //query es el string que vamos a recibir y que usaremos para buscar
   buscarGifs ( query: string ) {
 
-    this._historial.unshift( query );
+    //formateamos a minusculas
+    query = query.trim().toLowerCase();
+
+    //No añadimos los vacios
+    if ( query === '') return;
+    
+    //Para no añadir duplicados
+    if ( !this._historial.includes ( query) ){
+      this._historial.unshift( query );
+    }
+    //Para estraer solo los diez últimos
+    this._historial = this._historial.splice(0,10);
     console.log(this._historial);
   }
   constructor() { }
