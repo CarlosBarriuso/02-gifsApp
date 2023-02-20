2a4101bd7c8eae62f6e841fe46ca2e4d7ef2771f
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-20 11:16:55 +0100

    @ViewChild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -8,9 +8,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {
 
   //Para obtener información de un elemento html
-  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  //ViewChild localiza una etiqueta que tenga la referencia local txtBuscar
+  // el nombre que le ponemos al ViewChild es txtBuscar que  va a ser de tipo HTMLInputElement
+  //Par permitir que sea null se usa !
+  //Como ElementRef es de tipo generico hay que especificar, hay que especificar de que tipo es para poder acceder a sus metodos, por ejemplo el .value
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>; 
 
-  buscar(termino: string){
+  //ternimoBusqueda solo nos permite acceder a su valor y no podemos modificarlo
+  buscar(){ // buscar(terminoBusqueda: string){
     const valor = this.txtBuscar.nativeElement.value;
     console.log(valor);
     this.txtBuscar.nativeElement.value = '';
